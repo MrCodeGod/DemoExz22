d8a5d8712a3b2e00f8fd7293fa024305cd6c8fef
Nikita Ostroushko <nekit.ostroushko@mail.ru>
2026-06-09 05:11:13 +0500

    Добавьте файлы проекта.

---
+++ ProductWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using DemoExz22.DataBase;
+using DemoExz22.Helpers;
+using DemoExz22.Statics;
+using DemoExz22.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemoExz22
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        private ShoeShopDBEntities db = new ShoeShopDBEntities();
+        private MessageHelper messageHelper = new MessageHelper();
+        private List<ProductViewModel> _productViewModels = new List<ProductViewModel>();
+
+        private string[] _sortOptions = new string[]
+        {
+            "По умолчанию",
+            "По убыванию",
+            "По возрастанию"
+        };
+
+        private List<string> _filterTypes = new List<string>()
+        {
+            "Все поставщики"
+        };
+
+        public ProductWindow()
+        {
+            InitializeComponent();
+            LoadProducts();
+            LoadData();
+            LoadUI();
+        }
+
+        private void LoadUI()
+        {
+            Users user = CurrentSession.CurrentUser;
+            if (user == null || user.RoleID == 3)
+            {
+                AdminPanel.Visibility = Visibility.Collapsed;
+            }
+            else if (user.RoleID == 1)
+            {
+                CreateButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void LoadData()
+        {
+            SortComboBox.ItemsSource = _sortOptions;
+            SortComboBox.SelectedIndex = 0;
+
+            var supplier = db.Supplier.ToList();
+            foreach (var item in supplier)
+            {
+                _filterTypes.Add(item.Name);
+            }
+            FilterComboBox.ItemsSource = _filterTypes;
+            FilterComboBox.SelectedIndex = 0;
+
+            Users user = CurrentSession.CurrentUser;
+            
+            if (user != null)
+            {
+                FullUserName.Text = $"{user.SurName} {user.FirstName} {user.Patronymic}";
+            }
+        }
+
+        private void LoadProducts()
+        {
+            _productViewModels = db.Product
+                .OrderBy(p => p.ID)
+                .ToList()
+                .Select(p => new ProductViewModel(p))
+                .ToList();
+            ProductList.ItemsSource = _productViewModels;
+        }
+
+        private void UpdateProducts()
+        {
+            ProductList.ItemsSource = null;
+            ProductList.ItemsSource = _productViewModels;
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentSession.CurrentUser = null;
+            new MainWindow().Show();
+            Close();
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ApplyFilters();
+        }
+
+        private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ApplyFilters();
+        }
+        private void FilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ApplyFilters();
+        }
+
+        private void ApplyFilters()
+        {
+            string searchText = SearchTextBox.Text.ToLower();
+
+            string supplierText = FilterComboBox.SelectedItem?.ToString();
+
+            int sortingType = SortComboBox.SelectedIndex;
+
+            var products = db.Product.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                products = products.Where(p =>
+                    p.Category.Name.ToLower().Contains(searchText) ||
+                    p.Name.ToLower().Contains(searchText) ||
+                    p.Description.ToLower().Contains(searchText) ||
+                    p.Supplier.Name.ToLower().Contains(searchText) ||
+                    p.Manufacturer.Name.ToLower().Contains(searchText) ||
+                    p.Unit.Name.ToLower().Contains(searchText)
+                );
+            }
+
+            if (!string.IsNullOrWhiteSpace(supplierText) &&
+                supplierText != "Все поставщики")
+            {
+                products = products.Where(p => p.Supplier.Name == supplierText);
+            }
+
+            if (sortingType == 1)
+            {
+                products = products.OrderByDescending(p => p.Quantity);
+            }
+            else if (sortingType == 2)
+            {
+                products = products.OrderBy(p => p.Quantity);
+            }
+            else
+            {
+                products = products.OrderBy(p => p.ID);
+            }
+
+            _productViewModels = products
+                .ToList()
+                .Select(p => new ProductViewModel(p))
+                .ToList();
+
+            UpdateProducts();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            new AddEditProductWindow(null).ShowDialog();
+            Close();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Users user = CurrentSession.CurrentUser;
+            if (user.RoleID != 1)
+            {
+                return;
+            }
+            else
+            {
+                int id = (int)(sender as Border).Tag;
+
+                new AddEditProductWindow(id).Show();
+                Close();
+            }
+        }
+    }
+}
